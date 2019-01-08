37f38090edc0e307adcc83fe404b58fa76e42a8f
asitkdash <asitdash.csu.fullerton.edu>
2019-01-08 11:30:59 -0800

    single and photo grid with action and reducers

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,28 @@
+// increment the likes
+
+export const increment = (index) => {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    }
+};
+// add comment
+
+export const addComment = (postId, author, comment) => {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+};
+
+//delete comment
+
+export const removeComment = (postId, i) => {
+    return {
+        type: 'REMOVE_COMMENT',
+        i,
+        postId
+    }
+};
