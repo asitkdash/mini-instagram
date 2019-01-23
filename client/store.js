e89754bce5dffad0f1d94b5ddbf562c7d56769d7
asitkdash <asitdash.csu.fullerton.edu>
2019-01-22 21:16:50 -0800

    redux dev tool with chrome

--- client/store.js
+++ client/store.js
@@ -14,9 +14,11 @@ const defaultState = {
   comments
 };
 
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
-
-const store = createStore(rootReducer, defaultState);
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
